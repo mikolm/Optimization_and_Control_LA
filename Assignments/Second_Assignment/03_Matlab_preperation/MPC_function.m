ca63f9db471ad066670dab420d664f654ad59b93
luto1998 <95966023+luto1998@users.noreply.github.com>
2022-01-26 18:37:39 +0100

    Add files via upload
    task 6 and plots are missing. A few things should also be improved

---
+++ Assignments/Second_Assignment/03_Matlab_preperation/MPC_function.m
@@ -0,0 +1,99 @@
+function delta_u_hat = MPC_function(input)
+    
+    % get Data from Workspace
+    H = evalin('base','H');
+    Hx = evalin('base','Hx');
+    Q = evalin('base','Q');
+    R = evalin('base','R');
+    p = evalin('base','p');
+    Np = evalin('base','Np');
+    m = evalin('base','m ');
+    Nc = evalin('base','Nc');
+    h = evalin('base','h');
+    
+    % decompose Input vector 
+    rk = input(1:Np*p);
+    gkx_hat = input((Np*p)+1:3*Np*p);
+    gk_hat= input((3*Np*p)+1:4*Np*p);
+    last_uk= input((4*Np*m)+1:end);
+        
+    
+    % prepare yalmip
+    yalmip('clear') % delete old yalmip variables
+    options = sdpsettings('verbose',0,'solver','qpoases','cachesolvers',1);
+    delta_u_hat = sdpvar(m*Nc,1);
+
+%% Constraints
+
+    % calculate L and M Matrizes for constraints
+    L=[];
+    for index = 1:Nc
+        L_loop = eye(m);
+        L = [L; L_loop];
+    end
+
+    M_row = [eye(m),zeros(m,m*(Nc-1))];
+    M = toeplitz(L(:,1), M_row(1,:));
+
+    
+    % actuatiting variable
+    u_min = 0;
+    u_max = 5;
+    actuating_constraint = [L * last_uk - u_min; u_max - L * last_uk];
+
+    % %change of the actuatiting variable
+    % delta_u_min = [];
+    % delta_u_max =[];
+    % actuating_change_constraint = [-delta_u_min;delta_u_max];
+
+    % %controlled variable
+    % y_hat_min = [];
+    % y_hat_max = [];
+    % controll_variable_constraint = [gk_hat - y_hat_min; y_hat_max - gk_hat];
+
+    %state variable
+    x_min = h;
+    xmin_vec = [];
+    for counter = 1:Np
+        
+        xmin_vec = [xmin_vec; x_min];
+    
+    end
+    
+    disp(xmin_vec)
+
+    x_max = h+ [20; 20];
+    xmax_vec = [];
+    for counter = 1:Np
+        
+        xmax_vec = [xmax_vec; x_max];
+    
+    end
+    
+    state_variable_constraint = [gkx_hat - xmin_vec; xmax_vec - gkx_hat];
+
+
+
+    %w_hat = [actuating_constraint;actuating_change_constraint;controll_variable_constraint];
+    %E = eye(size(M));
+    %W = [-M; M ; -E ; E; H; -H];
+    
+    W = [-M; M; -Hx ; Hx];
+    w_hat = [actuating_constraint;state_variable_constraint];
+    constraints = W*delta_u_hat <= w_hat;
+%%
+
+    ek_hat = gk_hat -rk;
+    
+    % Objective Function
+    J = delta_u_hat' * (H'*Q*H+R) * delta_u_hat + 2*delta_u_hat'* H' * Q * ek_hat;
+            
+    diagnostics = optimize(constraints,J,options);
+    if diagnostics.problem > 0
+        error('Error during optimization')
+    end
+
+    delta_u_hat_vec = value(delta_u_hat);
+    delta_u_hat = delta_u_hat_vec(1);
+
+end
--- Assignments/Second_Assignment/03_Matlab_preperation/Task1_Funcs.m
+++ Assignments/Second_Assignment/03_Matlab_preperation/Task1_Funcs.m
@@ -1,81 +1,18 @@
 classdef Task1_Funcs
     methods(Static)
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        function [l] = perfrom_optimization(phi, domega, g, norm)
+        function [u_eq,x_eq] = equilibrium(alpha,beta,gamma,k,x2_eq)
 
-            yalmip('clear') % delete old yalmip variables
+            syms u1_eq x1_eq
 
-            A = sin(phi);
+            eq_system = [ % first ODE
+                          k(1)*sqrt(x1_eq) == alpha(1) + sqrt( beta(1)+gamma(1)*u1_eq ), ...
+                          % second ODE
+                          k(2)*sqrt(x2_eq) == alpha(1) + sqrt( beta(1)+gamma(1)*u1_eq )];
 
-            phat = sdpvar(1,1); % Declare optimization variables
-
-            e = domega-A*phat; % Error
-
-            bounds = sdpvar(length(e),1);
-            constraints = [-bounds <= e <= bounds];
-            options = sdpsettings('verbose',0);
-
-            % what optimization is performed
-            if strcmp(norm,'L1')
-                diagnostics = optimize(constraints,sum(bounds),options)    
-            elseif strcmp(norm,'L2')
-                diagnostics = optimize([],e'*e,options)       
-            elseif strcmp(norm,'Linfty')
-                diagnostics = optimize(constraints,max(bounds),options)        
-            else
-                error('ERROR: wrong setting for the cost function');
-            end
-
-            % error handling
-            if diagnostics.problem > 0
-                error('Error during optimization')
-            end
-
-            % calculate the unknown parameter based on the optimization variable
-            phat = value(phat); % Convert results in double variables
-            l = g/phat; % Compute results
-
-            disp(['Length l yielded by the ', norm, ' optimization: l= ', num2str(l), 'm']);
-        end
-        
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        function compare_phi(t_measurements, phi, phi_simu)
-            % plot the results in a figure
-            figure()
-                set(gcf, 'Units', 'Normalized', 'Position', [0, 0, 0.8, 0.9]);
-                %
-                subplot(3,1,1)
-                plot(t_measurements, phi, 'linewidth', 2);
-                hold on; box on; grid on;
-                plot(phi_simu.time(:), phi_simu.signals.values(:,1), '--', 'linewidth', 2);
-                title('Result of the $L_1$ Optimization', 'Interpreter', 'Latex','Fontsize', 14);
-                xlabel('t in s', 'Interpreter', 'Latex', 'Fontsize', 12); 
-                ylabel('$\varphi$ in rad', 'Interpreter', 'Latex','Fontsize', 12);
-                %ylim([0,2*pi]); yticks([0:pi/2:2*pi]);
-                %yticklabels({'0','pi/2','pi', '3/2 pi', '2pi'});
-                %
-                subplot(3,1,2)
-                plot(t_measurements, phi, 'linewidth', 2);
-                hold on; box on; grid on;
-                plot(phi_simu.time(:), phi_simu.signals.values(:,2), '--', 'linewidth', 2);
-                title('Result of the $L_2$ Optimization', 'Interpreter', 'Latex','Fontsize', 14);
-                xlabel('t in s', 'Interpreter', 'Latex', 'Fontsize', 12); 
-                ylabel('$\varphi$ in rad', 'Interpreter', 'Latex','Fontsize', 12);
-                %ylim([0,2*pi]); yticks([0:pi/2:2*pi]);
-                %yticklabels({'0','pi/2','pi', '3/2 pi', '2pi'});
-                %
-                subplot(3,1,3)
-                plot(t_measurements, phi, 'linewidth', 2);
-                hold on; box on; grid on;
-                plot(phi_simu.time(:), phi_simu.signals.values(:,3), '--', 'linewidth', 2);
-                title('test', 'Fontsize', 16); 
-                title('Result of the $L_\infty$ Optimization', 'Interpreter', 'Latex','Fontsize', 14);
-                xlabel('t in s', 'Interpreter', 'Latex', 'Fontsize', 12); 
-                ylabel('$\varphi$ in rad', 'Interpreter', 'Latex','Fontsize', 12);
-                %ylim([0,2*pi]); yticks([0:pi/2:2*pi]);
-                %yticklabels({'0','pi/2','pi', '3/2 pi', '2pi'});
-
+            S = solve(eq_system, [u1_eq, x1_eq]);
+            x_eq = [double(S.x1_eq), x2_eq];
+            u_eq = [double(S.u1_eq); 0];
         end
     end
 end
